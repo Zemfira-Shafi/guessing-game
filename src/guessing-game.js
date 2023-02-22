033225e103e23b4c722c7a64609c06126ca054bd
Zemfira-Shafi <zemulya1@yandex.ru>
2023-02-22 21:12:09 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.num = Math.round((this.min + this.max) / 2);
+    return this.num;
+  }
 
-    guess() {
+  lower() {
+    this.setRange(this.min, this.num);
+    this.guess();
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.setRange(this.num, this.max);
+    this.guess();
+  }
 }
 
 module.exports = GuessingGame;
